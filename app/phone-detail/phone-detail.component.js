03e475e4926a20711949e3f1867b76bdb46e45d7
Luiz Fernando Maehana <luizmaehana@gmail.com>
2024-05-01 09:40:27 -0300

    12 - Event Handlers

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -8,8 +8,14 @@ angular.
     controller: ['$http', '$routeParams',
       function PhoneDetailController($http, $routeParams) {
         var self = this;
+
+        self.setImage = function setImage(imageUrl) {
+          self.mainImageUrl = imageUrl;
+        };
+
         $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
           self.phone = response.data;
+          self.setImage(self.phone.images[0]);
         });
       }
     ]
